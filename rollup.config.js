ed4cf5c7aa58a33fbc21cb67caf2e9062b356ba0
cihad <cihadp@gmail.com>
2019-07-14 17:16:49 +0300

    new formats added

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,53 @@
 import vue from 'rollup-plugin-vue'
 import commonjs from 'rollup-plugin-commonjs';
 
-export default {
-  input: 'src/components/Header.vue',
-  output: {
-    format: 'umd',
-    file: 'dist/Header.js',
-    name: "Header"
+export default [
+  {
+    input: 'src/components/Header.vue',
+    output: {
+      format: 'iife',
+      file: 'dist/Header.iife.js',
+      name: "Header"
+    },
+    plugins: [
+      vue(),
+      commonjs(),
+    ]
   },
-  plugins: [
-    vue(),
-    commonjs(),
-  ]
-}
+  {
+    input: 'src/components/Header.vue',
+    output: {
+      format: 'umd',
+      file: 'dist/Header.umd.js',
+      name: "Header"
+    },
+    plugins: [
+      vue(),
+      commonjs(),
+    ]
+  },
+  {
+    input: 'src/components/Header.vue',
+    output: {
+      format: 'cjs',
+      file: 'dist/Header.cjs.js',
+      name: "Header"
+    },
+    plugins: [
+      vue(),
+      commonjs(),
+    ]
+  },
+  {
+    input: 'src/components/Header.vue',
+    output: {
+      format: 'amd',
+      file: 'dist/Header.amd.js',
+      name: "Header"
+    },
+    plugins: [
+      vue(),
+      commonjs(),
+    ]
+  }
+]
